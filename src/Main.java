3e19a70a074489351ca72aa09fd3789e2598e36b
Alibi <sabitovalibi@gmail.com>
2025-07-29 16:01:20 +0500

    Finaleee_i_think

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        TaskManager manager = new TaskManager();
+
+        // Создаем эпик
+        Epic epic = new Epic("Переезд", "Организовать переезд в другой офис");
+        manager.createEpic(epic);
+
+        // Создаем подзадачи для эпика
+        SubTask subtask1 = new SubTask("Собрать коробки", "Упаковать вещи", Status.NEW, epic.getId());
+        SubTask subtask2 = new SubTask("Перевезти вещи", "Доставить в новый офис", Status.NEW, epic.getId());
+        manager.createSubtask(subtask1);
+        manager.createSubtask(subtask2);
+
+        // Создаем обычную задачу
+        Task task = new Task("Позвонить клиенту", "Обсудить договор", Status.IN_PROGRESS);
+        manager.createTask(task);
+
+        // Выводим все задачи
+        System.out.println("Все задачи:");
+        manager.getAllTasks().forEach(System.out::println);
+
+        System.out.println("\nВсе эпики:");
+        manager.getAllEpics().forEach(System.out::println);
+
+        System.out.println("\nВсе подзадачи:");
+        manager.getAllSubtasks().forEach(System.out::println);
+
+        // Меняем статус подзадачи и проверяем статус эпика
+        subtask1.setStatus(Status.DONE);
+        manager.updateSubtask(subtask1);
+
+        System.out.println("\nСтатус эпика после изменения подзадачи: " + epic.getStatus());
     }
 }
---
+++ src/SubTask.java
@@ -0,0 +1,28 @@
+public class SubTask extends Task {
+    private int epicId; // ID эпика, к которому относится подзадача
+
+    public SubTask(String name, String description, Status status, int epicId) {
+        super(name, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    public void setEpicId(int epicId) {
+        this.epicId = epicId;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                ", epicId=" + epicId +
+                '}';
+    }
+}
